0ba93624ed172cf27db181b79c8d89dd3ae52e16
EddyPoet <EddyHua@outlook.com>
2019-11-18 00:51:38 +0800

    A SSH B 免密登录

--- shell_script/shell_script.sh
+++ shell_script/shell_script.sh
@@ -18,5 +18,14 @@ sleep 10s
 #nohup sh ./shell_script.sh >/dev/null 2>&1 & #丢弃所有信息
 #nohup sh ./shell_script.sh >/dev/null 2>errlog & #仅输出错误信息
 
-
+# A SSH B 免密登录
+#A 生成公钥/私钥对(/root/test/id_rsa.pub | /root/.ssh/id_rsa)。
+ssh-keygen -t rsa -P ''
+#A -> B
+scp /root/.ssh/id_rsa.pub root@B_host:/root/test/id_rsa.pub
+#B
+cat /root/test/id_rsa.pub >> /root/.ssh/authorized_keys
+chmod 600 /root/.ssh/authorized_keys
+#test A ssh B
+ssh B_host
 
